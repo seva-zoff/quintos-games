f176aeab7dcce99d938bea38c8859dd637cba3b1
seva-zoff <seva.zoff@gmail.com>
2022-05-31 18:07:03 +0300

    1.1.5

---
+++ GAMES/QuickClicks/quickClicks.js
@@ -0,0 +1,20 @@
+const target = `
+ .d88b. 
+.8P  Y8.
+88    88
+88    88
+ 8b  d8 
+ 'Y88P' `.slice(1);
+// slice removes the first character from the String
+// in this case I remove the new line at the beginning
+// so the first line of the button will be at the proper
+// row value
+
+/* Part A: find the range of row and column values the target can be placed at */
+function makeTargets() {
+	let row = 1;
+	let col = 1;
+	button(target, row, col);
+}
+
+makeTargets();
--- load.js
+++ load.js
@@ -1,3 +1,3 @@
-QuintOS.username = 'seva-zoff'; // put your github username!
+QuintOS.user = 'seva-zoff'; // put your github username!
 QuintOS.dir = 'GAMES'; // the folder with the game folder in it
-QuintOS.gameTitle = 'Hangman'; // the title of the game to load
+QuintOS.game = 'QuickClicks'; // the title of the game to load
