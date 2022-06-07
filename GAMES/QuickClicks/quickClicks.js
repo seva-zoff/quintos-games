372946d27d2154497ed5d9f113a8a54d405bf2dc
seva-zoff <seva.zoff@gmail.com>
2022-06-07 18:09:41 +0300

    1.1.6

--- GAMES/QuickClicks/quickClicks.js
+++ GAMES/QuickClicks/quickClicks.js
@@ -5,16 +5,48 @@ const target = `
 88    88
  8b  d8 
  'Y88P' `.slice(1);
-// slice removes the first character from the String
-// in this case I remove the new line at the beginning
-// so the first line of the button will be at the proper
-// row value
+
+const imposter = `
+ .d88b. 
+.8P  Y8.
+88 xx 88
+88 xx 88
+ 8b  d8 
+ 'Y88P' `.slice(1);
+
+let targetRow;
+let targetCol;
 
 /* Part A: find the range of row and column values the target can be placed at */
-function makeTargets() {
-	let row = 1;
-	let col = 1;
-	button(target, row, col);
+
+async function imposterClicked() {
+	erase();
+	text(target, targetRow, targetCol);
+	await alert('Game Over!', 20, 30, 20);
+	erase();
+	startGame();
 }
 
-makeTargets();
+function normalClicked() {
+	erase();
+	makeTargets();
+}
+
+function makeTargets() {
+	targetRow = round(random(1, 23));
+	targetCol = round(random(1, 71));
+	button(target, targetRow, targetCol, normalClicked);
+
+	for (let i = 0; i < 4; i++) {
+		let row = round(random(1, 23));
+		let col = round(random(1, 71));
+		button(imposter, row, col, imposterClicked);
+	}
+}
+
+async function startGame() {
+	await alert('You need to click to normal targets, if you will click to imposter you will lose', 11);
+	makeTargets();
+}
+
+startGame();
