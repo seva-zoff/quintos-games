c9c886e1d11a597e1365351bfb1de66992e8545c
seva-zoff <seva.zoff@gmail.com>
2022-06-21 18:28:01 +0300

    1.1.7

--- GAMES/QuickClicks/quickClicks.js
+++ GAMES/QuickClicks/quickClicks.js
@@ -17,6 +17,8 @@ const imposter = `
 let targetRow;
 let targetCol;
 
+let times = [];
+
 /* Part A: find the range of row and column values the target can be placed at */
 
 async function imposterClicked() {
@@ -27,12 +29,69 @@ async function imposterClicked() {
 	startGame();
 }
 
-function normalClicked() {
+function showSpeedStats() {
 	erase();
-	makeTargets();
+
+	let speeds = [];
+
+	for (let i = 0; i < times.length - 1; i++) {
+		speeds[i] = times[i + 1] - times[i];
+	}
+
+	log(speeds);
+
+	// let sum = speeds[0] + speeds[1] + speeds[2] ...
+
+	// speeds = [500, 1500, 200]
+	let sum = 0;
+	for (let i = 0; i < speeds.length; i++) {
+		sum = sum + speeds[i];
+	}
+	log(sum); // -> 2200
+	let avg = Math.round(sum / speeds.length);
+
+	let slowest = speeds[0];
+	let fastest = speeds[0];
+	for (let i = 1; i < speeds.length; i++) {
+		if (slowest < speeds[i]) {
+			slowest = speeds[i];
+		}
+		if (fastest > speeds[i]) {
+			fastest = speeds[i];
+		}
+	}
+
+	alert(
+		'Your average speed was: ' +
+			avg +
+			'ms\n' +
+			'Your slowest speed was: ' +
+			slowest +
+			'ms\n' +
+			'Your fastest speed was :' +
+			fastest +
+			'ms',
+		11
+	);
+}
+
+function normalClicked() {
+	times.push(Date.now());
+	log(times);
+	if (times.length != 10) {
+		erase();
+		makeTargets();
+	} else {
+		showSpeedStats();
+	}
 }
 
 function makeTargets() {
+	let pattern = '-_/\\_'.repeat(15) + '-_/';
+	for (let i = 1; i < 30; i += 3) {
+		text(pattern, i, 1);
+	}
+
 	targetRow = round(random(1, 23));
 	targetCol = round(random(1, 71));
 	button(target, targetRow, targetCol, normalClicked);
